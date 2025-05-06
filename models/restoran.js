fb47dd8c92e6ae150ee0ba3fc896ce7ab8c133f2
Klosar92 <gasahdja@gmail.com>
2025-05-06 20:27:22 +0200

    Create restoran.js

---
+++ models/restoran.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const RestoranSchema = new mongoose.Schema(
+  {
+    ime:       String,
+    adresa:    String,
+    lat:       Number,
+    lon:       Number,
+     loc: {
+          type:        { type: String, enum: ['Point'], default: 'Point', required: true },
+          coordinates: { type: [Number],               required: true }
+        },
+    kategorija:String,
+    vrsta:     String,
+    region:    String
+  },
+  { collection: 'restorans' }   // <── KLJUČNI RED
+);
+
+RestoranSchema.index({ loc: '2dsphere' });
+module.exports = mongoose.model('Restoran', RestoranSchema);
